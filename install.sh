91cca5ac7758f3bd3302125799e2da225dc75f67
Metin Balaban <balaban@ucsd.edu>
2022-06-08 14:53:05 -0700

    install script bug

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 if conda info --envs | grep "fastloo" > /dev/null; then
         echo "conda environment fastloo exists"
 else
-        conda create -y -c bioconda -c conda-forge  python=3.8 pip gappa=0.7.1 numpy
+        conda create -y -c bioconda -c conda-forge --name fastloo  python=3.8 pip gappa=0.7.1 numpy
 	source activate fastloo
         conda activate fastloo
         pip install apples==2.0.10
